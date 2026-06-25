fbf8ad75633cfe5af3a791627b44489ee544a534
Kenneth Blake <kgb66@drexel.edu>
2026-06-25 13:47:43 -0400

    mark

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
