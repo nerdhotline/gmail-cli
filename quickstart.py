2a28f5383041fdfd0779fcaf550b6733f466cc5f
Kenneth Blake <kgb66@drexel.edu>
2026-07-08 22:59:05 -0400

    rehash

--- quickstart.py
+++ quickstart.py
@@ -42,9 +42,12 @@ def collectMessages(creds):
   service = build("gmail", "v1", credentials=creds)
   results = (
     service.users().messages().list(
-      userId="me", labelIds=["INBOX"]
+      userId="me", 
+      q="category:primary",
+      labelIds=["INBOX"]
     ).execute()
   )
+
   messages = results.get("messages", [])
 
   # TODO: test no messages catch
@@ -60,33 +63,50 @@ def main():
   creds = grabCredentials()
   try:
     messages, service = collectMessages(creds)
-
-
     
     for message in messages:
       msg = (
         service.users().messages().get(
           userId="me", 
-          id=message["id"]
+          id=message["id"],
+          format="full"
         ).execute()
       )
 
-      if(msg["payload"]["body"]["size"] == 0):
-        continue
-      print(f'Message ID: {message["id"]}')
+      # # CUSTOM HEADER DATA
+      header = msg["payload"]["headers"]
+      wantedValues = ["Date", "Subject", "From", "To", "Sender", "Reply-To"]
+      cstmHeader = {}
+
+      for itm in header:
+        if itm["name"] in wantedValues:
+          cstmHeader[itm["name"].lower()] = itm["value"]
       
+      cstmHeader["id"] = msg["id"]
+      cstmHeader["threadId"] = msg["threadId"]
+      cstmHeader["labelIds"] = msg["labelIds"]
+      cstmHeader["snippet"] = msg["snippet"]
+
+      print(json.dumps(msg, indent=2))
+
       raw_body_data = msg["payload"]["body"]["data"]
+
+
+
       decoded_bytes = base64.urlsafe_b64decode(raw_body_data)
       decoded_text = decoded_bytes.decode('utf-8').strip()
       decoded_text = re.sub(r'<!-- == Footer Section == -->[\s\S]*', '', decoded_text)
 
       h = html2text.HTML2Text()
       h.ignore_links = True
+      markdown_text = html2text.html2text(decoded_text)
 
       console = Console(width=100)
-      markdown_text = html2text.html2text(decoded_text)
+      console.print(cstmHeader["subject"])
+      console.rule()
       console.print(Panel(Markdown(markdown_text.replace("|", ''), )))
-      print(f'  Subject: {msg["snippet"]}')
+
+      exit(7)
 
   except HttpError as error:
     # TODO(developer) - Handle errors from gmail API.
